44f78eaa5ca0b5855bf7bcdd4da304dbc3559f9d
NecroticPhantom <167430021+NecroticPhantom@users.noreply.github.com>
2024-07-17 13:08:10 +0100

    added notation

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-//make some accounts have admin priviledges
-function accountManagment() {
+//make sure local storage works
+function accountManagment() { //make an account with admin privileges
     accountManaging = True;
     while (accountManaging) {
         let loginOrCreate = window.prompt("Login or Create Account? ");
