419d2ac16674d010597fd51d0e57e75bcd8207cf
NecroticPhantom <167430021+NecroticPhantom@users.noreply.github.com>
2024-07-18 13:48:38 +0100

    made encryptPassword acctually a function (I'm an idiot)

--- js/passwordProtection.js
+++ js/passwordProtection.js
@@ -1,7 +1,7 @@
 const currentURL = window.location;
 if (currentURL == "https://necroticphantom.github.io/To-Do-List/login" || currentURL == "https://necroticphantom.github.io/To-Do-List/create-account")
 window.onload = encryptPassword();
-encryptPassword() {
+function encryptPassword() {
     const passwordInput = document.getElementById("myInput");
     passwordInput.addEventListener("keyup", (event) => {
         const password = event.target.value;
