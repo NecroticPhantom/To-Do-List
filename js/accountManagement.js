61b05171b5ea3592f1aff881f5b93b11f10ac764
NecroticPhantom <167430021+NecroticPhantom@users.noreply.github.com>
2024-07-18 13:22:12 +0100

    changed submit to button so eventlisteners work

--- js/accountManagement.js
+++ js/accountManagement.js
@@ -25,10 +25,10 @@ function accountManagement() { //make an account with admin privileges
     };
 };
 if (currentURL == "https://necroticphantom.github.io/To-Do-List/create-account") { //change - to %20 (space)???
-    document.getElementById("signUpSubmit").addEventListener("click", signUp);
+    document.getElementById("signUpButton").addEventListener("click", signUp);
 }
 else if (currentURL == "https://necroticphantom.github.io/To-Do-List/login") { //change - to %20 (space)???
-    document.getElementById("loginSubmit").addEventListener("click", login);
+    document.getElementById("loginButton").addEventListener("click", login);
 };
 function validateEmail(testEmail) {
     return emailRegex.test(String(testEmail));
