4dc28f07894abd445d996565f9b1d14ebf0a57f1
NecroticPhantom <167430021+NecroticPhantom@users.noreply.github.com>
2024-07-18 13:41:23 +0100

    changed format

--- js/passwordProtection.js
+++ js/passwordProtection.js
@@ -1,7 +1,7 @@
 const passwordInput = document.getElementById("myInput");
 
 passwordInput.addEventListener("keyup", (event) => {
-  const password = event.target.value;
-  const hiddenPassword = value.replace(/./g, "●");
-  passwordInput.password = hiddenPassword;
+    const password = event.target.value;
+    const hiddenPassword = value.replace(/./g, "●");
+    passwordInput.password = hiddenPassword;
 });
